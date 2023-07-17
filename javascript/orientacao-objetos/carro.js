fc98bb5d971f9879ea43aa138b692e607732b55e
DAVI NUNES SARAIVA <a95067895@rn.senac.local>
2023-07-17 19:52:24 -0300

    orientacao a objetos

---
+++ javascript/orientacao-objetos/carro.js
@@ -0,0 +1,18 @@
+export default class Carro {
+
+    // atributos
+    marca;
+    modelo;
+    ano;
+
+   // constructor 
+   constructor(modelo, marca, ano) {
+       this.modelo = modelo;
+       this.marca = marca;
+       this.ano = ano;
+
+   }
+
+ // metodos
+
+}
